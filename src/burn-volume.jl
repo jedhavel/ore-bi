54352e5427f97985e9d5294f945de2217ef4cf4d
BuildTools <unconfigured@null.spigotmc.org>
2021-10-26 12:42:18 -0500

    Add data, instantburn and cumulative burn charts

---
+++ src/burn-volume.jl
@@ -0,0 +1,18 @@
+#! julia
+
+# run from ore-bi top dir
+using PlotlyJS, CSV, DataFrames, Glob
+
+datafiles = CSV.File.(glob("data/ore-token-burn-*.csv")) #Get all the token burn data files
+dfs = DataFrame.(datafiles) # Convert to DataFrames
+
+function _append!(dataframes::Vector{})
+    for i = 1:size(dataframes)[1] - 1 # size returns tuple, we want the 1st value
+        append!(dataframes[i], dataframes[i+1])
+    end
+end
+
+_append!(dfs)
+p_instantburn = plot(dfs[1], x=:DateTime, y=:Quantity, Layout(title="Instant Burn"))
+
+p_cumulativeburn = plot(dfs[1], x=:DateTime, y=:CumulativeQuantity, Layout(title="Cumulative Burn"))
